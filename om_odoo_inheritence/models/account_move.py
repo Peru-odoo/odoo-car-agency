fef5186a80e387e1fecb60701dea6b4a7b281eff
jihedha1 <jihedhallem1234@gmail.com>
2024-07-29 15:38:05 +0100

    Delete om_odoo_inheritence directory

--- om_odoo_inheritence/models/account_move.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class AccountMove(models.Model):
-
-    _inherit = 'account.move'
-
-    so_confirmed_user_id = fields.Many2one('res.users', string='So Confirmed User')
--- om_odoo_inheritence/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class SaleOrder(models.Model):
-
-    _inherit = 'sale.order'
-
-    confirmed_user_id = fields.Many2one('res.users', string='Confirmed User')
